7005f1076a9f44e5bd8e05ce463ff97752892139
Kachi_d_light <53799172+onukafavour@users.noreply.github.com>
2026-01-22 13:44:54 +0100

    Create Stage_Zero_code.R
    This is a simple code for assigning variables and printing them out

---
+++ Stage_Zero_code.R
@@ -0,0 +1,15 @@
+#My names
+Names = "Onuka Favour"
+
+#My university/Affiliation
+Uni_Affiliation = "Redeemer's University"
+
+
+#My gene of interest
+Fav_gene = "IL-6"
+
+#Organisms the gene is found
+org = "Animalia"
+
+#Code to print it out as a sentence
+paste0("My name is ", Names, ", An MSc student at the Prestigious ", Uni_Affiliation, ". ", "My Favorite gene is ", Fav_gene, " in ", org, ".") 
